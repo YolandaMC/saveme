566d1d6b1227ea251e67535fc8784f630a910d74
YolandaMC <onandno.project@gmail.com>
2022-05-25 13:31:03 +0200

    move without rotation and correction in finish

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -8,30 +8,22 @@ public class Move: MonoBehaviour //esa clase creada llamada Move tiene que llama
     [SerializeField] Transform groundCheck; 
     [SerializeField] LayerMask ground;
 
-    [SerializeField] float movementSpeed = 6f; //movement speed
-    [SerializeField] float rotationSpeed = 200f;
+    [SerializeField] float movementSpeed = 200f; //movement speed
+    //[SerializeField] float rotationSpeed = 200f;
     [SerializeField] float jumpForce = 5f; //jump force
-    private Animator anim;
+    //[SerializeField] public Animator anim;
 
     [SerializeField] AudioSource jumpSound; // jump sound
 
     //[SerializeField] Vector3 velocidad; //variable vector velocidad para el desplazamiento
 
-    //[SerializeField] float velRotation;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //velocidad = new Vector3(1.0f, 2.0f, 3.0f); //he creado una velocidad que yo consigo que modifique la velocidad pero no me ayuda a que no siga dando saltos el programa
-        //el programa da saltos por el numero de fotogramas y cada dispositivo trabaja a unos fotogramas luego necesito multiplicar la velocidad por un atributo que me regule
-        //esos fotogramas por vuelta/loop/update. Para ello uso deltaTime
-        //ahora me doy cuenta que con esta velocidad establecida el desplazamiento es muy lento. Para no tener que estar modificando codigo constantemente voy a hacer este atributo
-        //p�blico, es decir, que lo pueda modificar desde unity (modificado en tiempo de ejecuion), pero cuidado luego si tienes que ponerlo definitivo en el archivo script, lo otro solo es de testeo
-        //por ultimo comento el new o inicializacion de la velocidad porque lo he hehco modificable desde la interfaz (y asi no tengo que venir aqui)
-        //ahora hay otra cuestion, hacer publicos atributos puede dar error, entonces unity da otra opcion a hacerlo publico, SerializeField permite que sea privado pero el desarrollador
-        //lo modifique desde la interfaz de unity
-        anim = GetComponent<Animator>();
+        
+        //anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
     }
 
@@ -42,11 +34,11 @@ public class Move: MonoBehaviour //esa clase creada llamada Move tiene que llama
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Rotate(0, horizontalInput*Time.deltaTime*rotationSpeed, 0) //esta funcion hace que el personaje rote
-        transform.Translate(new Vector3(0.0f, 0.0f, verticalInput * movementSpeed.x * Time.deltaTime));
+        //transform.Rotate(0, horizontalInput*Time.deltaTime*rotationSpeed, 0); //esta funcion hace que el personaje rote
+        transform.Translate(new Vector3(verticalInput*Time.deltaTime*movementSpeed, 0.0f, horizontalInput*Time.deltaTime*movementSpeed));
 
-        anim.SetFloat("VelX", x);
-        anim.SetFloat("VelY", y);
+        //anim.SetFloat("VelX", horizontalInput);
+        //anim.SetFloat("VelY", verticalInput);
 
         //rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
         
