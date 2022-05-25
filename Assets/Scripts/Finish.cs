707b6d705c5c96042575bea7635567aa510366d3
YolandaMC <onandno.project@gmail.com>
2022-05-25 10:37:52 +0200

    scripts añadidos

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] GameObject UINuevoJuego; //public GameObject UINuevoJuego;
-    [SerializeField] GameObject UIGameplay; //public GameObject UIGameplay;
-
-    [SerializeField] Manager Estado; //public Manager Estado;
-
-    public void btnNuevoJuego (){
-        //UINuevoJuego.SetActive(false);
-        //UIGameplay.SetActive(true);
-        //Estado.STATE_MACHINE = "PLAY";
+    public void StartGame() //para el botón que me permita ir a la escena de juego
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame() //para salir por completo de la aplicacion
+    {
+        Application.Quit();
+    }
+
+
+    // [SerializeField] GameObject UINuevoJuego; //public GameObject UINuevoJuego;
+    // [SerializeField] GameObject UIGameplay; //public GameObject UIGameplay;
+
+    // [SerializeField] Manager Estado; //public Manager Estado;
+
+    // public void btnNuevoJuego (){
+    //     //UINuevoJuego.SetActive(false);
+    //     //UIGameplay.SetActive(true);
+    //     //Estado.STATE_MACHINE = "PLAY";
+    // }
 }
